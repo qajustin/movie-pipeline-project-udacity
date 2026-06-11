f9e36a6f9424811c3520870dbf6fe7a35a766d7e
Justin Tambong <qajustintambong@gmail.com>
2026-06-12 05:25:26 +0800

    Update __init__.py

--- starter/backend/__init__.py
+++ starter/backend/__init__.py
@@ -1,11 +1,11 @@
-import os
 from flask import Flask
-from flask_cors import CORS
 
-from .movies import movies_api
+try:
+    from .movies import movies_api
+except ImportError:
+    from movies import movies_api
 
 app = Flask(__name__)
-CORS(app)
 app.register_blueprint(movies_api)
 
 # Start app
