1f51918502d1cc2fedbf4341d422a0f93343579e
Justin Tambong <qajustintambong@gmail.com>
2026-06-12 05:51:14 +0800

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,15 +6,26 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    });
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
+      .then((response) => {
+        console.log('API response:', response.data);
+        setMovies(response.data?.movies || []);
+      })
+      .catch((error) => {
+        console.error('Error fetching movies:', error);
+        setMovies([]);
+      });
   }, []);
 
   return (
     <ul>
-      {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+      {(movies || []).map((movie) => (
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
